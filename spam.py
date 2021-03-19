62f3ed7176f7e52f1aee0aced78f978b29c2e88d
Ari k <75741608+ArikSquad@users.noreply.github.com>
2021-03-19 20:07:24 +0200

    Updated for more info!

--- spam.py
+++ spam.py
@@ -9,4 +9,5 @@ for word in f:
     pyautogui.typewrite(word)
     pyautogui.press("enter")
     time.sleep(1)
+    # we want to use time.sleep(1) because some apps doesnt allow spamming so this will bypass
     
